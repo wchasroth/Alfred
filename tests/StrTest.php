5510071c49b43954f0e29892c799f959c6e9a42b
Charles Roth <croth@thedance.net>
2024-11-16 11:50:39 -0500

    Add more tests, learn about IgnoreDeprecations attribute

--- tests/StrTest.php
+++ tests/StrTest.php
@@ -3,6 +3,7 @@
 
    use PHPUnit\Framework\TestCase;
    use PHPUnit\Framework\Attributes\Test;
+   use PHPUnit\Framework\Attributes\IgnoreDeprecations;
 
    use charlesroth_net\Alfred\Str;
 
@@ -16,17 +17,44 @@
       }
 
       #[Test]
-      public function shouldGetNothingForNonexistentSubstring(): void {
+      public function shouldGetNothing_forNonexistentAfterString(): void {
          self::assertEmpty (Str::substringAfter("abcdef", "x"));
       }
 
-      #[Test]
-      public function shouldHandleNulls(): void {
-         self::assertEmpty("",   Str::substringAfter(null, null));
-         self::assertEmpty("",   Str::substringAfter(null, "x"));
+      #[Test] #[IgnoreDeprecations]  // ignore null warnings
+      public function shouldHandleNullsForAfter(): void {
+         self::assertEmpty(Str::substringAfter(null, null));
+         self::assertEmpty(Str::substringAfter(null, "x"));
 
          self::assertSame("abc", Str::substringAfter("abc", null));
          self::assertSame("abc", Str::substringAfter("abc", ""));
+      }
+
+      //---substringBefore()
+      #[Test]
+      public function shouldExtractSubstringBefore(): void {
+         self::assertSame ("ab",  Str::substringBefore("abcdef", "c"));
+         self::assertSame ("abc", Str::substringBefore("abcdef", "de"));
+      }
+
+      #[Test]
+      public function shouldGetOriginalString_forNonexistentBeforeString(): void {
+         self::assertSame ("abcdef", Str::substringBefore("abcdef", "x"));
+      }
+
+      #[Test] #[IgnoreDeprecations]  // ignore null warnings
+      public function shouldHandleNullsForBefore(): void {
+         self::assertEmpty(Str::substringBefore(null, null));
+         self::assertEmpty(Str::substringBefore(null, "x"));
+
+         self::assertEmpty(Str::substringBefore("abc", null));
+         self::assertEmpty(Str::substringBefore("abc", ""));
+      }
+
+      //---substringBetween()
+      #[Test]
+      public function shouldExtractSubstringBetween(): void {
+         self::assertSame ("cd",  Str::substringBetween("abcdef", "b", "e"));
       }
 
    }
