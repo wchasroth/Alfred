eacd651f8eed83ea452fdb48f5685d0c80e17e2d
Charles Roth <croth@thedance.net>
2025-01-14 16:29:32 -0500

    Clean/deduplicate UrlChecker

--- src/UrlChecker.php
+++ src/UrlChecker.php
@@ -25,6 +25,7 @@ class UrlChecker {
     //  002       no info available
     //  003       parked      (domain parked, but no content)
     //  403       http forbidden
+    //  405       http method not allowed, but often means page does not exist or is parked, and can't response with headers.
     //  436       http bad identity
     //  NNN       other http error
     //  900       Facebook page doesn't seem to exist
@@ -34,24 +35,7 @@ class UrlChecker {
         $urlLower = strtolower($url);
         if (Str::contains($urlLower, "facebook.com/"))   return UrlChecker::checkFacebookPage($url);
 
-//        // Generic website checking here, uses headers to determine existence.
-//        stream_context_set_default( [
-//            'ssl' => [
-//                'verify_peer' => false,
-//                'verify_peer_name' => false,
-//            ],
-//            'http' => ['Accept' => '', 'Accept-Encoding' => '']
-//        ]);
-//        $headers = @get_headers($url);
-//        if ($headers === false)  return "001";
-//
-//        echo "\n";
-//        echo "$url\n";
-//        foreach ($headers as $header) {
-//            echo "   $header\n";
-//        }
         $headers = UrlChecker::getHeadersOnly($url);
-        echo "Has 200? " . UrlChecker::headersHas200($headers) . "\n";
 
         if (UrlChecker::headersHas200($headers)) {
             return ! UrlChecker::isParkedDomain($url, $headers) ? "200" : "003";
@@ -66,6 +50,14 @@ class UrlChecker {
     }
 
     private static function getHeadersOnly(string $url): array {
+        $handle = UrlChecker::makeCurlHandleFor($url);
+        curl_setopt($handle, CURLOPT_NOBODY, true);
+        $response = curl_exec($handle);
+        curl_close($handle);
+        return Str::split($response, "\n");
+    }
+
+    private static function makeCurlHandleFor($url) {
         $header = [
             'User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12',
             'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -81,13 +73,8 @@ class UrlChecker {
         curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($handle, CURLOPT_COOKIESESSION, false);
         curl_setopt($handle, CURLOPT_HEADER, true);
-        curl_setopt($handle, CURLOPT_NOBODY, true);
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($handle);
-        curl_close($handle);
-        $result = Str::split($response, "\n");
-        print_r($result);
-        return $result;
+        return $handle;
     }
 
     private static function checkFacebookPage(string $url): string {
@@ -97,7 +84,6 @@ class UrlChecker {
     }
 
     private static function isParkedDomain(string $url): bool {
-//        $random = @get_headers($url . "/someRandomImpossiblePageUri123789");
         $random = UrlChecker::getHeadersOnly($url . "/someRandomImpossiblePageUri123789");
         return UrlChecker::headersHas200($random);   // Almost always means entire domain is parked!
     }
@@ -110,24 +96,8 @@ class UrlChecker {
     }
 
     private static function getTextFromUrl(string $url): string {
-        $header = [
-            'User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12',
-            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language: en-us,en;q=0.5',
-            'Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7',
-            'Keep-Alive: 115',
-            'Connection: keep-alive',
-        ];
-        $handle = curl_init($url);
-        curl_setopt($handle, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($handle, CURLOPT_COOKIESESSION, false);
-
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        $handle = UrlChecker::makeCurlHandleFor($url);
         $response = curl_exec($handle);
-
         curl_close($handle);
         return $response;
     }
