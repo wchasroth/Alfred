289c0d1e34ba9ec682c2c17cfcf426a6af5146f2
Charles Roth <croth@thedance.net>
2025-01-24 14:57:58 -0500

    Refactor GoogleAuthHelper so it can be tested

--- src/GoogleAuthHelper.php
+++ src/GoogleAuthHelper.php
@@ -6,52 +6,59 @@ use CharlesRothDotNet\Alfred\Str;
 use \Exception;
 
 class GoogleAuthHelper {
-    public static function makeGoogleUserProfile($clientId, $clientRedirectUrl, $clientSecret, $code): GoogleUserProfile {
-        $env  = new EnvFile("_env");
-        $logger = new DumbFileLogger($env->get('logFile'));
-        $data = GoogleAuthHelper::getAccessToken($clientId, $clientRedirectUrl, $clientSecret, $code, $logger);
-        $accessToken = $data['access_token'];
-        if (Str::startsWith($accessToken, 'Error:'))  return new GoogleUserProfile("", $accessToken);
+   private string $accessTokenUrl;
+   private string $userInfoUrl;
 
-        $user_info = GoogleAuthHelper::getUserProfileInfo($accessToken);
-        $email = $user_info['email'];
-        return new GoogleUserProfile($email, "");
-    }
+   public function __construct(string $accessTokenUrl, string $userInfoUrl) {
+      $this->accessTokenUrl = $accessTokenUrl;
+      $this->userInfoUrl = $userInfoUrl;
+   }
 
-    private static function getAccessToken(string $client_id, string $redirect_uri, string $client_secret, string $code, $logger) {
-        $url = 'https://www.googleapis.com/oauth2/v4/token';
+   public static function makeDefaultGoogleAuthHelper(): GoogleAuthHelper {
+      return new GoogleAuthHelper("https://www.googleapis.com/oauth2/v4/token",
+         "https://www.googleapis.com/oauth2/v2/userinfo?fields=name,email,gender,id,picture,verified_email");
+   }
 
-        $curlPost = 'client_id=' . $client_id . '&redirect_uri=' . $redirect_uri . '&client_secret=' . $client_secret . '&code='. $code . '&grant_type=authorization_code';
-        $logger->log("curlPost=" . $curlPost);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
-        $logger->log("about to exec");
-        $data = json_decode(curl_exec($ch), true);
-        $logger->log("after exec");
-        $logger->log("data: " . print_r($data, true));
-        $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);
-        $logger->log("http code=" . $http_code);
-        return ($http_code == 200 ? $data : ['access_token' => 'Error : Failed to receive access token']);
-    }
+   public function makeGoogleUserProfile($clientId, $clientRedirectUrl, $clientSecret, $code, $logger): GoogleUserProfile {
+      $data = $this->getAccessToken($clientId, $clientRedirectUrl, $clientSecret, $code, $logger);
+      $accessToken = $data['access_token'];
+      if (Str::startsWith($accessToken, 'Error:')) return new GoogleUserProfile("", $accessToken);
 
-    private static function getUserProfileInfo($access_token) {
-        $url = 'https://www.googleapis.com/oauth2/v2/userinfo?fields=name,email,gender,id,picture,verified_email';
+      $user_info = $this->getUserProfileInfo($accessToken);
+      $email = $user_info['email'];
+      return new GoogleUserProfile($email, "");
+   }
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '. $access_token));
-        $data = json_decode(curl_exec($ch), true);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        if ($http_code != 200)
-            throw new Exception('Error : Failed to get user information');
+   function getAccessToken(string $client_id, string $redirect_uri, string $client_secret, string $code, $logger) {
+      $curlPost = 'client_id=' . $client_id . '&redirect_uri=' . $redirect_uri . '&client_secret=' . $client_secret . '&code=' . $code . '&grant_type=authorization_code';
+      $logger->log("curlPost=" . $curlPost);
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $this->accessTokenUrl);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+      $logger->log("about to exec");
+      $data = json_decode(curl_exec($ch), true);
+      $logger->log("after exec");
+      $logger->log("data: " . print_r($data, true));
+      $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      $logger->log("http code=" . $http_code);
+      return ($http_code == 200 ? $data : ['access_token' => 'Error : Failed to receive access token']);
+   }
 
-        return $data;
-    }
+   function getUserProfileInfo($access_token) {
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $this->userInfoUrl);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $access_token));
+      $data = json_decode(curl_exec($ch), true);
+      $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      if ($http_code != 200)
+         throw new Exception('Error : Failed to get user information');
+
+      return $data;
+   }
 
 }
---
+++ tests/GoogleAuthHelperTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+use CharlesRothDotNet\Alfred\GoogleAuthHelper;
+use CharlesRothDotNet\Alfred\Str;
+use CharlesRothDotNet\Alfred\DumbFileLogger;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\IgnoreDeprecations;
+
+use CharlesRothDotNet\Alfred\AlfredPDO;
+use CharlesRothDotNet\Alfred\PdoRunResult;
+
+class GoogleAuthHelperTest extends TestCase {
+
+   #[Test]
+   public function shouldSimulateGettingAccessToken() {
+      $logger = new DumbFileLogger("C:/WAMP/www/Alfred/log");
+      $helper = new GoogleAuthHelper("http://localhost/Alfred/htdocs/simAccessToken.php",
+         "http://localhost/Alfred/htdocs/simUserProfile.php");
+      $data = $helper->getAccessToken("client_id", "redirect_uri", "client_secret", "code", $logger);
+      self::assertEquals("abcdef", $data['access_token']);
+   }
+
+   #[Test]
+   public function shouldSimulateMakingUserProfile() {
+      $logger = new DumbFileLogger("C:/WAMP/www/Alfred/log");
+      $helper = new GoogleAuthHelper("http://localhost/Alfred/htdocs/simAccessToken.php",
+         "http://localhost/Alfred/htdocs/simUserProfile.php");
+      $userProfile = $helper->makeGoogleUserProfile("client_id", "redirect_uri", "client_secret", "code", $logger);
+      self::assertTrue($userProfile->succeeded());
+      self::assertEquals ("wchasroth@gmail.com", $userProfile->getEmail());
+   }
+
+//user_info: Array
+//(
+//[id] => 101790690783650220217
+//[email] => wchasroth@gmail.com
+//[verified_email] => 1
+//[name] => Charles Roth
+//[p
+}
