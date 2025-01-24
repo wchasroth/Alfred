002e75549f503994727801d6762826986936a07e
Charles Roth <croth@thedance.net>
2025-01-24 16:06:06 -0500

    Allow for no-op DumbFileLogger

--- src/DumbFileLogger.php
+++ src/DumbFileLogger.php
@@ -4,19 +4,20 @@ namespace CharlesRothDotNet\Alfred;
 
 use \Exception;
 
-class DumbFileLogger
-{
-    private string $filename;
+class DumbFileLogger {
+   private string $filename;
 
-    public function __construct(string $file) {
-        $this->filename = $file;
-    }
+   public function __construct(string $file) {
+      $this->filename = $file;
+   }
 
-    public function log(string $text) {
-        $fp = fopen($this->filename, 'a');
-        if ($fp === false)  throw new Exception("Cannot write to $this->filename");
-        fwrite($fp, $text . "\n");
-        fclose($fp);
-    }
+   public function log(string $text) {
+      if (empty($this->filename)) return;
+
+      $fp = fopen($this->filename, 'a');
+      if ($fp === false) throw new Exception("Cannot write to $this->filename");
+      fwrite($fp, $text . "\n");
+      fclose($fp);
+   }
 
 }
--- tests/DumbFileLoggerTest.php
+++ tests/DumbFileLoggerTest.php
@@ -9,26 +9,34 @@ use PHPUnit\Framework\Attributes\Test;
 
 class DumbFileLoggerTest extends TestCase {
 
-    #[Test]
-    public function shouldCatchAttemptToLog_toNonExistingDirectoryAndFile(): void {
-        $logger = new DumbFileLogger("/noSuchDirectory/NorFile");
-        $error = "";
-        try                  { $logger->log("test"); }
-        catch (Exception $e) { $error = $e->getMessage(); }
-        self::assertTrue(Str::contains($error, "Cannot write to /noSuchDirectory/NorFile"));
-    }
+   #[Test]
+   public function shouldCatchAttemptToLog_toNonExistingDirectoryAndFile(): void {
+      $logger = new DumbFileLogger("/noSuchDirectory/NorFile");
+      $error = "";
+      try   { $logger->log("test"); }
+      catch (Exception $e) { $error = $e->getMessage(); }
+      self::assertTrue(Str::contains($error, "Cannot write to /noSuchDirectory/NorFile"));
+   }
 
-    #[Test]
-    public function shouldLogToFile(): void {
-        $filename = getcwd() . "/unittest.tmp";
-        unlink($filename);
+   #[Test]
+   public function shouldLogToFile(): void {
+      $filename = getcwd() . "/unittest.tmp";
+      unlink($filename);
 
-        $logger = new DumbFileLogger($filename);
-        $logger->log("test");
+      $logger = new DumbFileLogger($filename);
+      $logger->log("test");
 
-        $contents = file_get_contents($filename);
-        unlink($filename);
-        self::assertSame ("test\n", $contents);
-    }
+      $contents = file_get_contents($filename);
+      unlink($filename);
+      self::assertSame("test\n", $contents);
+   }
+
+   #[Test]
+   public function shouldDoNothingGivenNoFileName() {
+      $logger = new DumbFileLogger("");
+      try   { $logger->log("test"); }
+      catch (Exception $e) { self::fail("should not get here"); }
+      self::assertTrue(true);
+   }
 
 }
