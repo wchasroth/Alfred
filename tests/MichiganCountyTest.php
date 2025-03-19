8fa33339670dd568816ac4e7755c8da9bf949f08
Charles Roth <croth@thedance.net>
2025-03-19 17:19:16 -0400

    Add MichiganCounty

---
+++ tests/MichiganCountyTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+use CharlesRothDotNet\Alfred\MichiganCounty;
+
+class MichiganCountyTest extends TestCase {
+
+   #[Test]
+   public function shouldExerciseGetId() {
+      self::assertEquals (1,  MichiganCounty::getId("Alcona"));
+      self::assertEquals (1,  MichiganCounty::getId("Alcona "));
+      self::assertEquals (0,  MichiganCounty::getId("OuterSpace"));
+      self::assertEquals (0,  MichiganCounty::getId(""));
+
+      self::assertEquals (75, MichiganCounty::getId("St Joseph"));
+   }
+
+   #[Test]
+   public function shouldExerciseGetName() {
+      self::assertEquals ("ALCONA",    MichiganCounty::getName(1));
+      self::assertEquals ("",          MichiganCounty::getName(-1));
+      self::assertEquals ("",          MichiganCounty::getName(84));
+
+      self::assertEquals ("ST JOSEPH", MichiganCounty::getName(75));
+
+   }
+
+}
