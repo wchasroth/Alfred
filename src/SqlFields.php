5744e11219e49063cf09a87629bdd5f157621a06
Charles Roth <croth@thedance.net>
2025-05-14 23:32:16 -0400

    Make AlfredPDO and SqlFields use prepared statements (:key form) behind the scenes, reducing chance of SQL injection

--- src/SqlFields.php
+++ src/SqlFields.php
@@ -4,36 +4,65 @@ declare(strict_types=1);
 namespace CharlesRothDotNet\Alfred;
 
 class SqlFields {
-   private array $fields;
+   private array $keyValuePairs;
 
-   function __construct(array $fields) {
-      $this->fields = $fields;
+   function __construct(array $keyValuePairs) {
+      $this->keyValuePairs = $keyValuePairs;
+   }
+
+   public function getKeyValuePairs(): array {
+      return $this->keyValuePairs;
    }
 
    public function getSelectFragment(): string {
       return $this->getFragmentWithSeparator(" AND ");
    }
 
+   public function makeSelectFragment(): string {
+      return $this->makeFragmentWithSeparator(" AND ");
+   }
+
    public function getUpdateFragment(): string {
       return $this->getFragmentWithSeparator(", ");
+   }
+
+   public function makeUpdateFragment(): string {
+      return $this->makeFragmentWithSeparator(", ");
    }
 
    public function getInsertFragment(): string {
       $names  = [];
       $values = [];
-      foreach ($this->fields as $key => $value) {
+      foreach ($this->keyValuePairs as $key => $value) {
          $names[]  = $key;
          $values[] = (is_int($value) ? $value : Str::singleQuoted($value));
       }
       return " (" . Str::join($names, ", ") . ") VALUES (" . Str::join($values, ", ") . ") ";
+   }
 
+   public function makeInsertFragment(): string {
+      $names  = [];
+      $values = [];
+      foreach ($this->keyValuePairs as $key => $value) {
+         $names[]  =   $key;
+         $values[] = ":$key";
+      }
+      return " (" . Str::join($names, ", ") . ") VALUES (" . Str::join($values, ", ") . ") ";
    }
 
    private function getFragmentWithSeparator(string $separator): string {
       $results = [];
-      foreach ($this->fields as $key => $value) {
+      foreach ($this->keyValuePairs as $key => $value) {
          $results[] = "$key = " . (is_int($value) ? $value : Str::singleQuoted($value));
       }
       return " " . Str::join($results, $separator) . " ";
    }
+
+   private function makeFragmentWithSeparator(string $separator): string {
+      $results = [];
+      foreach ($this->keyValuePairs as $key => $value)  $results[] = "$key = :$key";
+      return " " . Str::join($results, $separator) . " ";
+   }
+
+
 }
--- tests/SqlFieldsTest.php
+++ tests/SqlFieldsTest.php
@@ -15,15 +15,35 @@ class SqlFieldsTest extends TestCase {
    }
 
    #[Test]
+   public function shouldMakeSelectFragment_givenFields() {
+      $keyValues = ["abc" => 123, "def" => "xyz"];
+      $fields = new SqlFields($keyValues);
+      self::assertEquals (" abc = :abc AND def = :def ", $fields->makeSelectFragment());
+      self::assertEquals ($keyValues, $fields->getKeyValuePairs());
+   }
+
+   #[Test]
    public function shouldGetUpdateFragment_givenFields() {
       $fields = new SqlFields(["abc" => 123, "def" => "xyz"]);
       self::assertEquals (" abc = 123, def = 'xyz' ", $fields->getUpdateFragment());
    }
 
    #[Test]
+   public function shouldMakeUpdateFragment_givenFields() {
+      $fields = new SqlFields(["abc" => 123, "def" => "xyz"]);
+      self::assertEquals (" abc = :abc, def = :def ", $fields->makeUpdateFragment());
+   }
+
+   #[Test]
    public function shouldGetInsertFragment_givenFields() {
       $fields = new SqlFields(["abc" => 123, "def" => "xyz"]);
       self::assertEquals (" (abc, def) VALUES (123, 'xyz') ", $fields->getInsertFragment());
+   }
+
+   #[Test]
+   public function shouldMakeInsertFragment_givenFields() {
+      $fields = new SqlFields(["abc" => 123, "def" => "xyz"]);
+      self::assertEquals (" (abc, def) VALUES (:abc, :def) ", $fields->makeInsertFragment());
    }
 
 }
