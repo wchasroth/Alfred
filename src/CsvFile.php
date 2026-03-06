124767facc68878f77f029cb44e0e3bb81b8df86
Charles Roth <croth@thedance.net>
2026-03-06 15:33:25 -0500

    CsvFile->loadfile: strip leading # off of 1st line

--- src/CsvFile.php
+++ src/CsvFile.php
@@ -23,6 +23,7 @@ class CsvFile {
       rewind($fp);
 
       $this->keys = fgetcsv($fp, 0, $separator);
+      if (Str::startsWith($this->keys[0], "#"))  $this->keys[0] = Str::substringAfter($this->keys[0], "#");
       rewind($fp);
 
       $this->rows = [];
